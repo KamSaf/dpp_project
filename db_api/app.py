bea3bd633b8a60d36d9a2a7ed43ae00b82aea986
KamSaf <bopaf42@gmail.com>
2025-12-15 00:20:07 +0100

    Refactor environment variables in db_api

--- db_api/app.py
+++ db_api/app.py
@@ -3,14 +3,14 @@ import uvicorn
 from src.config import engine
 from src.models import Base
 
-host = os.getenv("CONTAINER_API_HOST")
-port = os.getenv("CONTAINER_API_PORT")
+host = os.getenv("CONTAINER_API_HOST", "localhost")
+port = int(os.getenv("CONTAINER_API_PORT", "8000"))
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
     uvicorn.run(
         "src.main:app",
         reload=True,
-        host=host if host else "0.0.0.0",
-        port=int(port) if port else 8000,
+        host=host,
+        port=port,
     )
